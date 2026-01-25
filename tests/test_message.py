a4375e126e9d74d16228aeea8932a65bccd6f303
Josh Ferguson <Josh@spartan-plumbing.com>
2026-01-24 23:44:31 -0500

    fix(tests): ensure message module import

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,6 +1,12 @@
 import importlib
+import os
+import sys
 import types
+
 import pytest
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 
 @pytest.fixture(scope="module")
