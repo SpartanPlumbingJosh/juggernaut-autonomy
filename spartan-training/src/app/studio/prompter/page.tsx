e61abca5ec29d8a38e0122f0fb625ee08eac1361
SpartanPlumbingJosh <143583599+SpartanPlumbingJosh@users.noreply.github.com>
2026-03-28 15:05:53 -0400

    feat: add Teleprompter page

---
+++ spartan-training/src/app/studio/prompter/page.tsx
@@ -0,0 +1,7 @@
+import { Teleprompter } from "@/components/studio/Teleprompter";
+
+export const dynamic = "force-dynamic";
+
+export default function PrompterPage() {
+  return <Teleprompter />;
+}
