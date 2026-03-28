666431e32a3a334f0fc912f4bf4fefc23b2a2bcf
SpartanPlumbingJosh <143583599+SpartanPlumbingJosh@users.noreply.github.com>
2026-03-28 15:05:46 -0400

    feat: add Script Generator page

---
+++ spartan-training/src/app/studio/scripts/page.tsx
@@ -0,0 +1,7 @@
+import { ScriptGenerator } from "@/components/studio/ScriptGenerator";
+
+export const dynamic = "force-dynamic";
+
+export default function ScriptsPage() {
+  return <ScriptGenerator />;
+}
